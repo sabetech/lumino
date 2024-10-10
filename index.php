c64cf8199feb1c6daf61bc9a0476ee9505bb503b
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-10-10 22:53:44 +0000

    Commit example

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">Dashboard</h1>
+            <h1 class="page-header">Dashboard - Make A change</h1>
             <div class="container">
                 <div class="row">
                     <div class="col-xs-6 col-md-3 col-lg-3" style="padding: 2em;">
